ed4c9e06e04ae38519a4d06eac76caf61bff8da8
enricbaixauli <enricbaixaulicasan@gmail.com>
2026-02-18 13:11:55 +0100

    ...

--- scikit-opt-master/scikit-opt-master/examples/demo_ga.py
+++ scikit-opt-master/scikit-opt-master/examples/demo_ga.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sys
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 def schaffer(p):
     '''
@@ -39,32 +41,89 @@ class GA_t(GA):
 # 3 experiments, 1 per a cada paràmetre (pop size, mutation vals, tournament) modifiquem el valor de dit paràmetre 
 # i mantenim els valors estàndard (els del mig) pals demès, 30 voltes cada
 #  or grid search with these values
-best_fitnesses=[]
-size_pop_vals=[20, 50, 1000]
-prob_mut_vals=[0.25, 0.01, 0.001]
-tournament_vals=[1, 3, 10] # modified in selection.py or we can create a way to do it directly from here
-precision_vals=[0.2, 0.01, 0.001]
-
 #do a loop for averaging the same combination of parameters average of the best fitness and average of the average
-for j in range(3):
-    for i in range(30):
-        ga = GA_t(func=schaffer, n_dim=2, size_pop=size_pop_vals[j], max_iter=100, prob_mut=prob_mut_vals[1], lb=[-1, -1], ub=[1, 1], precision=precision_vals[1], tournament_size=tournament_vals[1])
-
-        best_x, best_y = ga.run()
-        print('Config ', size_pop_vals[j], ' ', 'best_x:', best_x, '\n', 'best_y:', best_y)
-        best_fitnesses.append(best_y)
 
 
 
-avg_fitness=np.mean(best_fitnesses)
-std_fitness=np.std(best_fitnesses)
-print('avg_fitness:', avg_fitness, '\n', 'std_fitness:', std_fitness)
+# best_fitnesses=[]
+# size_pop_vals=[20, 50, 1000]
+# prob_mut_vals=[0.25, 0.01, 0.001]
+# tournament_vals=[1, 3, 10] # modified in selection.py or we can create a way to do it directly from here
+# precision_vals=[0.2, 0.01, 0.001]
+
+# for j in range(3):
+#     for i in range(30):
+#         ga = GA_t(func=schaffer, n_dim=2, size_pop=size_pop_vals[j], max_iter=100, prob_mut=prob_mut_vals[1], lb=[-1, -1], ub=[1, 1], precision=precision_vals[1], tournament_size=tournament_vals[1])
+
+#         best_x, best_y = ga.run()
+#         print('Config ', size_pop_vals[j], ' ', 'best_x:', best_x, '\n', 'best_y:', best_y)
+#         best_fitnesses.append(best_y)
+
+
+
+# avg_fitness=np.mean(best_fitnesses)
+# std_fitness=np.std(best_fitnesses)
+# print('avg_fitness:', avg_fitness, '\n', 'std_fitness:', std_fitness)
+
+
+# Valors a testejar
+default_pop = 50
+default_mut = 0.01
+default_tourn = 3
+default_prec = 0.01
+
+experiments = {
+    "size_pop": [20, 50, 1000],
+    "prob_mut": [0.1, 0.01, 0.001],
+    "tournament_size": [1, 3, 10],
+    "precision": [0.1, 0.01, 0.001]
+}
+
+results = []
+
+for param_name, values in experiments.items():
+    print(f"Analizing: {param_name}")
+    
+    for val in values:
+        current_params = {
+            "size_pop": default_pop,
+            "prob_mut": default_mut,
+            "tournament_size": default_tourn,
+            "precision": default_prec
+        }
+        
+        current_params[param_name] = val
+        
+        best_fitnesses = []
+        
+        for i in range(30):
+            ga = GA_t(func=schaffer, n_dim=2, max_iter=100, lb=[-100, -100], ub=[100, 100], 
+                      **current_params)
+            best_x, best_y = ga.run()
+            best_fitnesses.append(best_y)
+        
+        avg_fitness = np.mean(best_fitnesses)
+        std_fitness = np.std(best_fitnesses)
+        
+        print(f"Config: {param_name}={val} -> Avg: {avg_fitness:.6f}, Std: {std_fitness:.6f}")
+        
+        results.append({
+            "Parameter": param_name,
+            "Value": val,
+            "Average Best Fitness": avg_fitness,
+            "Std Dev": std_fitness
+        })
+
+# Convertir a DataFrame per veure la taula final
+df_results = pd.DataFrame(results)
+print("\nResults:")
+print(df_results)
+
 
 # %% Plot the result
-import pandas as pd
-import matplotlib.pyplot as plt
 
-Y_history = pd.DataFrame(ga.all_history_Y)
+
+Y_history = pd.DataFrame(results)
 fig, ax = plt.subplots(2, 1)
 ax[0].plot(Y_history.index, Y_history.values, '.', color='red')
 Y_history.min(axis=1).cummin().plot(kind='line')
